253b124b6a4b1a17cc87846aba00cfc6204ce8d7
audiebant-dev <stuart.cornes@audiebant.co.uk>
2024-06-06 11:45:53 +0100

    Delete audiesafe_db.php

--- audiesafe_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-mysqli_report(MYSQLI_REPORT_OFF);
-$conn = mysqli_connect("localhost", "audiebantcodev_audiesafe", "+(HYUmGdm(*M", "audiebantcodev_audiesafe");
-if (mysqli_connect_errno()) {
-echo mysqli_connect_error();
-exit();
-} 
-?>
